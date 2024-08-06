851ef96f77ba1a13f85a3516dc4656c6c06e4246
kumarh1776 <59850401+kumarh1776@users.noreply.github.com>
2024-08-06 10:28:07 -0400

    Add files via upload

--- listener.py
+++ listener.py
@@ -1,40 +1,34 @@
 from web3 import Web3
-from web3.contract import Contract
 from web3.providers.rpc import HTTPProvider
-from web3.middleware import geth_poa_middleware #Necessary for POA chains
+from web3.middleware import geth_poa_middleware # Necessary for POA chains
 import json
 from datetime import datetime
 import pandas as pd
 
 eventfile = 'deposit_logs.csv'
 
-def scanBlocks(chain,start_block,end_block,contract_address):
+def scanBlocks(chain, start_block, end_block, contract_address):
     """
     chain - string (Either 'bsc' or 'avax')
     start_block - integer first block to scan
     end_block - integer last block to scan
     contract_address - the address of the deployed contract
 
-	This function reads "Deposit" events from the specified contract, 
-	and writes information about the events to the file "deposit_logs.csv"
+    This function reads "Deposit" events from the specified contract,
+    and writes information about the events to the file "deposit_logs.csv"
     """
     if chain == 'avax':
-        api_url = f"https://api.avax-test.network/ext/bc/C/rpc" #AVAX C-chain testnet
-
-    if chain == 'bsc':
-        api_url = f"https://data-seed-prebsc-1-s1.binance.org:8545/" #BSC testnet
-
-    if chain in ['avax','bsc']:
-        w3 = Web3(Web3.HTTPProvider(api_url))
-        # inject the poa compatibility middleware to the innermost layer
-        w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+        api_url = "https://api.avax-test.network/ext/bc/C/rpc" # AVAX C-chain testnet
+    elif chain == 'bsc':
+        api_url = "https://data-seed-prebsc-1-s1.binance.org:8545/" # BSC testnet
     else:
-        w3 = Web3(Web3.HTTPProvider(api_url))
+        raise ValueError(f"Unsupported chain: {chain}")
 
-    DEPOSIT_ABI = json.loads('[ { "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "token", "type": "address" }, { "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "Deposit", "type": "event" }]')
+    w3 = Web3(Web3.HTTPProvider(api_url))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+    DEPOSIT_ABI = json.loads('[{ "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "token", "type": "address" }, { "indexed": true, "internalType": "address", "name": "recipient", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "Deposit", "type": "event" }]')
     contract = w3.eth.contract(address=contract_address, abi=DEPOSIT_ABI)
-
-    arg_filter = {}
 
     if start_block == "latest":
         start_block = w3.eth.get_block_number()
@@ -42,14 +36,17 @@ def scanBlocks(chain,start_block,end_block,contract_address):
         end_block = w3.eth.get_block_number()
 
     if end_block < start_block:
-        print( f"Error end_block < start_block!" )
-        print( f"end_block = {end_block}" )
-        print( f"start_block = {start_block}" )
+        print(f"Error: end_block < start_block!")
+        print(f"end_block = {end_block}")
+        print(f"start_block = {start_block}")
+        return
 
     if start_block == end_block:
-        print( f"Scanning block {start_block} on {chain}" )
+        print(f"Scanning block {start_block} on {chain}")
     else:
-        print( f"Scanning blocks {start_block} - {end_block} on {chain}" )
+        print(f"Scanning blocks {start_block} - {end_block} on {chain}")
+
+    all_events = []
 
     if end_block - start_block < 30:
         event_filter = contract.events.Deposit.create_filter(fromBlock=start_block, toBlock=end_block)
